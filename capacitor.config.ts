199885dde03b64fbe2219a9bf5a7d527bc3a1804
Metehan Alp Saral <me@metehansaral.com>
2025-06-24 22:01:06 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'adaav-wetmap',
+  webDir: 'www'
+};
+
+export default config;
